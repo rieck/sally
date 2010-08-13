8076ae2ddff2e5e41de7a82e3a4f1534936ce0f2
Konrad Rieck <konrad.rieck@tu-berlin.de>
2010-08-13 13:49:37 +0200

    added import templates

---
+++ src/import.c
@@ -0,0 +1,34 @@
+/*
+ * Sally - A Tool for Embedding Strings in a Vector Space
+ * Copyright (C) 2010 Konrad Rieck (konrad@mlsec.org)
+ * --
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.  This program is distributed without any
+ * warranty. See the GNU General Public License for more details. 
+ */
+
+/** 
+ * @defgroup fvec Import functions
+ *
+ * Functions for importing feature vectors and other stuff.
+ *
+ * @author Konrad Rieck (konrad@mlsec.org)
+ * @{
+ */
+
+#include "config.h"
+#include "common.h"
+#include "util.h"
+#include "fhash.h"
+#include "import.h"
+
+fvec_t **import_fvec_dir(sally_t *sa, char *path, int *len)
+{
+    fvec_t **fv = NULL;
+    
+    return fv;
+}
+
+/** @} */
