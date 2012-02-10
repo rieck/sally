1bd56db96c7545ecfc36b0a1785f36828124b730
Konrad Rieck <konrad@mlsec.org>
2012-02-10 10:44:18 +0100

    Wrong branch! Revert "first cleanup of lib dir"
    This reverts commit f03c19d1f1e8d47aa5d109e2cda26561efffeac6.

---
+++ src/libsally/norm.h
@@ -0,0 +1,19 @@
+/*
+ * Sally - A Tool for Embedding Strings in Vector Spaces
+ * Copyright (C) 2010 Konrad Rieck (konrad@mlsec.org)
+ * --
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.  This program is distributed without any
+ * warranty. See the GNU General Public License for more details. 
+ */
+
+#ifndef NORM_H
+#define NORM_H
+
+#include "fvec.h"
+
+void fvec_norm(fvec_t *fv, const char *);
+
+#endif                          /* NORM_H */
