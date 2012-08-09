d85db154110a37b8c4a007bdff1b017f409e56af
Alexander Bikadorov <abiku@cs.tu-berlin.de>
2012-08-09 15:22:57 +0200

    added stdin input module

---
+++ src/input/input_stdin.h
@@ -0,0 +1,20 @@
+/*
+ * Sally - A Tool for Embedding Strings in Vector Spaces
+ * Copyright (C) 2010 Konrad Rieck (konrad@mlsec.org)
+ * --
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.  This program is distributed without any
+ * warranty. See the GNU General Public License for more details. 
+ */
+
+#ifndef INPUT_STDIN_H
+#define INPUT_STDIN_H
+
+/* Stdin module */
+int input_stdin_open(void);
+int input_stdin_read(string_t *, int);
+void input_stdin_close(void);
+
+#endif /* INPUT_STDIN_H */
