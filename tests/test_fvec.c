37175d0c8a8c287f3187387614cace1086900a57
chwress <chwress@github>
2012-08-22 15:39:23 +0200

    add sum test for arithmetic operations with spares features (this is far from complete)

--- tests/test_fvec.c
+++ tests/test_fvec.c
@@ -50,6 +50,15 @@ test_t tests[] = {
 /* String length */
 #define STR_LENGTH              1024
 
+
+void init_sally(test_t t)
+{
+    config_set_string(&cfg, "features.ngram_delim", t.dlm);
+    config_set_int(&cfg, "features.ngram_len", t.nlen);
+    fvec_delim_set(t.dlm); /* usually done in sally_init */
+}
+
+
 /* 
  * A simple static test for the feature vectors
  */
@@ -61,9 +70,7 @@ int test_static()
     test_printf("Extraction of feature vectors");
 
     for (i = 0; tests[i].str; i++) {
-        config_set_string(&cfg, "features.ngram_delim", tests[i].dlm);
-        config_set_int(&cfg, "features.ngram_len", tests[i].nlen);
-        fvec_delim_set(tests[i].dlm); /* usually done in sally_init */
+        init_sally(tests[i]);
 
         /* Extract features */
         f = fvec_extract(tests[i].str, strlen(tests[i].str));
@@ -79,6 +86,73 @@ int test_static()
 
     test_return(err, i);
     return err;
+}
+
+int test_arithmetic()
+{
+	int i = 0, err = 0;
+	double d1, d2 = 0.0;
+    fvec_t *fv1, *fv2, *empty;
+
+	test_printf("Arithmetic operations for feature vectors");
+
+    init_sally(tests[0]);
+    fv1 = fvec_extract(tests[0].str, strlen(tests[0].str));
+    empty = fvec_extract("", 0);
+
+	/* Multiplication with an empty feature vector */ i++;
+    fv2 = fvec_clone(fv1);
+
+    fvec_times(fv2, empty);
+    fvec_sparsify(fv2);
+    if (fv2->len != 0) {
+    	err++;
+    	test_error("(%d) len %d != 0", i, fv2->len);
+    }
+
+    fvec_destroy(fv2);
+
+	/* Addition with an empty feature vector */ i++;
+    fv2 = fvec_clone(fv1);
+
+    fvec_add(fv2, empty);
+    if (0 /*fvec_compare(fv1, fv2) */) {
+    	err++;
+    	test_error("(%d) addition failed!", i);
+    }
+
+    fvec_destroy(fv2);
+
+	/* Dot product with an empty feature vector */ i++;
+    d1 = fvec_dot(empty, fv1);
+    d2 = fvec_dot(fv1, empty);
+
+    if (d1 != 0 || d2 != 0) {
+    	err++;
+    	test_error("(%d) dot product failed!", i);
+    }
+
+	/* Scalar multiplication with 0 */ i++;
+    fv2 = fvec_clone(fv1);
+
+    fvec_mul(fv2, 0.0);
+    fvec_sparsify(fv2);
+    if (fv2->len != 0) {
+    	err++;
+    	test_error("(%d) scalar product failed!", i);
+    }
+
+    fvec_destroy(fv2);
+
+    /* TODO: Test all implemented arithmetic operations for
+     * sparse feature vectors
+     */
+
+    fvec_destroy(fv1);
+    fvec_destroy(empty);
+
+	test_return(err, i);
+	return err;
 }
 
 /* 
@@ -225,6 +299,7 @@ int main(int argc, char **argv)
     config_check(&cfg);
 
     err |= test_static();
+    err |= test_arithmetic();
     err |= test_stress();
 #ifdef ENABLE_OPENMP
     err |= test_stress_omp();
